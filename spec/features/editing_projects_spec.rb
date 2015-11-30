d4287b441027db9fdbcad860d99e4a4834020f38
Zaid Al Lahham <zlahham@gmail.com>
2015-11-30 23:43:10 +0000

    Projects can be updated

---
+++ spec/features/editing_projects_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can edit projects' do
+  before do
+    FactoryGirl.create(:project, name: 'Atom')
+    visit root_path
+    click_link 'Atom'
+    click_link 'Edit Project'
+  end
+
+  scenario 'with valid attributes' do
+    fill_in 'Name', with: 'Atom 2'
+    click_button 'Update Project'
+    expect(page).to have_content 'Project has been updated.'
+    expect(page).to have_content 'Atom 2'
+  end
+
+  scenario 'but not with invalid attributes' do
+    fill_in "Name", with: ""
+    click_button 'Update Project'
+    expect(page).to have_content 'Project has not been updated.'
+  end
+end
