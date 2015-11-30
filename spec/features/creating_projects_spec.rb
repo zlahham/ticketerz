bb0bed22ffa4837b8c88dcd7d2194ae0498f830e
Zaid Al Lahham <zlahham@gmail.com>
2015-11-30 18:51:08 +0000

    added dynamic title rendering for show pages

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -8,5 +8,11 @@ RSpec.feature 'Users can create new projects' do
     fill_in 'Description', with: 'A text editor for everyone'
     click_button 'Create Project'
     expect(page).to have_content 'Project has been successfully created'
+
+    project = Project.find_by(name: 'Atom')
+    expect(page.current_url).to eq project_url(project)
+
+    title = 'Atom - Projects'
+    expect(page).to have_title title
   end
 end
