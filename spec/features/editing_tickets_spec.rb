21478a0d13208a23ccfef08380084cf5ceb5c2b5
Zaid Al Lahham <zlahham@gmail.com>
2015-12-05 16:03:43 +0000

    tickets can be edited

---
+++ spec/features/editing_tickets_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can edit tickets' do
+  let(:project) { FactoryGirl.create(:project) }
+  let(:ticket) { FactoryGirl.create(:ticket, project: project) }
+
+  before do
+    visit project_ticket_path(project, ticket)
+    click_link 'Edit Ticket'
+  end
+
+  scenario 'with valid attributes' do
+    fill_in 'Name', with: 'Make it fun!'
+    click_button 'Update Ticket'
+    expect(page).to have_content 'Ticket has been updated.'
+    within("#ticket h2") do
+      expect(page).to have_content 'Make it fun!'
+      expect(page).not_to have_content ticket.name
+    end
+  end
+
+  scenario 'but not with invalid attributes' do
+    fill_in "Name", with: ""
+    click_button 'Update Ticket'
+    expect(page).to have_content 'Ticket has not been updated.'
+  end
+end
