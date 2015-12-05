e36e89630237c42657d136e2266079607dfb1b84
Zaid Al Lahham <zlahham@gmail.com>
2015-12-05 20:34:31 +0000

    user can sign in and see email after sign in

---
+++ spec/features/singing_in_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature 'Users that are signed up can sign in' do
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario 'when providing valid details' do
+    visit root_path
+    click_link 'Sign in'
+    fill_in 'Email', with: user.email
+    fill_in 'user_password', with: 'password'
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully.'
+    expect(page).to have_content "Signed in as #{user.email}"
+  end
+end
