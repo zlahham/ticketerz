78dfc91855d00564519edb24c6b6f736bea31546
Zaid Al Lahham <zlahham@gmail.com>
2015-12-05 16:15:44 +0000

    tickets can be deleted

---
+++ spec/features/deleting_tickets_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can delete their tickets' do
+  let(:project) { FactoryGirl.create(:project) }
+  let(:ticket) { FactoryGirl.create(:ticket, project: project) }
+
+  before { visit project_ticket_path(project, ticket) }
+
+  scenario 'successfully' do
+    click_link 'Delete Ticket'
+    expect(page).to have_content 'Ticket has been deleted.'
+    expect(page.current_url).to eq project_url(project)
+  end
+end
