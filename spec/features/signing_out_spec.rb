23aa1f1e2a2842e91c30cf5db65c321f87bf075f
Zaid Al Lahham <zlahham@gmail.com>
2015-12-05 20:42:53 +0000

    added sign out

---
+++ spec/features/signing_out_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can sign out' do
+  let!(:user) { FactoryGirl.create(:user) }
+  before { login_as(user) }
+
+  scenario 'when signed in' do
+    visit root_path
+    click_link 'Sign out'
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
