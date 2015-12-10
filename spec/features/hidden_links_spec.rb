7e084147c8e84a34a3d81ab28d0d26de31388a71
Zaid Al Lahham <zlahham@gmail.com>
2015-12-10 14:59:35 +0000

    only admins can see the delete and create links

---
+++ spec/features/hidden_links_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can only see the apropriate links' do
+  let(:project) { FactoryGirl.create(:project) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:admin) { FactoryGirl.create(:user, :admin) }
+
+  context 'anonymous user' do
+    scenario 'cannot see the New Project link' do
+      visit root_path
+      expect(page).not_to have_content 'New Project'
+    end
+
+    scenario 'cannot see the Delete Project link' do
+      visit project_path(project)
+      expect(page).not_to have_content 'Delete Project'
+    end
+  end
+
+  context 'regular user' do
+    before { login_as user }
+
+    scenario 'cannot see the New Project link' do
+      visit root_path
+      expect(page).not_to have_content 'New Project'
+    end
+
+    scenario 'cannot see the Delete Project link' do
+      visit project_path(project)
+      expect(page).not_to have_content 'Delete Project'
+    end
+  end
+
+  context 'admin user' do
+    before { login_as admin }
+
+    scenario 'can see the New Project link' do
+      visit root_path
+      expect(page).to have_content 'New Project'
+    end
+
+    scenario 'can see the Delete Project link' do
+      visit project_path(project)
+      expect(page).to have_content 'Delete Project'
+    end
+  end
+end
