b6e1c49d7b9fef5fc2eace12984c3f05dfc7a4ee
Zaid Al Lahham <zlahham@gmail.com>
2016-10-11 00:07:53 +0300

    Style fix

--- spec/features/editing_tickets_spec.rb
+++ spec/features/editing_tickets_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 feature 'Users can edit tickets' do
-  let(:author) { create(:user) }
+  let(:author)  { create(:user) }
   let(:project) { create(:project) }
-  let(:ticket) { create(:ticket, project: project, author: author) }
+  let(:ticket)  { create(:ticket, project: project, author: author) }
 
   before do
     visit project_ticket_path(project, ticket)
@@ -14,14 +14,14 @@ feature 'Users can edit tickets' do
     fill_in 'Name', with: 'Make it fun!'
     click_button 'Update Ticket'
     expect(page).to have_content 'Ticket has been updated.'
-    within("#ticket h2") do
+    within('#ticket h2') do
       expect(page).to have_content 'Make it fun!'
       expect(page).not_to have_content ticket.name
     end
   end
 
   scenario 'but not with invalid attributes' do
-    fill_in "Name", with: ""
+    fill_in 'Name', with: ''
     click_button 'Update Ticket'
     expect(page).to have_content 'Ticket has not been updated.'
   end
