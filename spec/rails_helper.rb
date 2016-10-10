503dcd14effc9a88c0e058b36d13f1603335bf6b
Zaid Al Lahham <zlahham@gmail.com>
2016-10-10 22:43:04 +0300

    Style fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 require 'spec_helper'
 require 'rspec/rails'
 require 'factory_girl_rails'
-
 
 ActiveRecord::Migration.maintain_test_schema!
 
