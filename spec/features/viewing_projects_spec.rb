a43a9c20b404f07bc0b490af5e8c7402cbc63a68
Zaid Al Lahham <zlahham@gmail.com>
2017-01-10 00:09:47 +0200

    Do not list projects that a user does not have permission to view

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
 
 feature 'Users can view all projects' do
-  let(:user)    { create(:user) }
-  let(:project) { create(:project, name: 'Atom') }
+  let(:user)      { create(:user) }
+  let(:project)   { create(:project, name: 'Atom') }
+  let!(:project2) { create(:project, name: 'Sublime') }
 
   before do
     login_as(user)
     assign_role!(user, :viewer, project)
+    visit root_path
   end
 
   scenario 'with the project details' do
-    visit root_path
     click_link 'Atom'
     expect(page.current_url).to eq project_url(project)
+  end
+
+  scenario 'unless they do not have permission' do
+    expect(page).not_to have_content(project2.name)
   end
 end
--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -1,10 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe ProjectPolicy do
-  let(:user)    { create(:user) }
-  let(:project) { create(:project) }
+  let(:user)     { create(:user) }
+  let!(:project) { create(:project) }
 
   subject { described_class }
+
+  context 'policy_scope' do
+    subject { Pundit.policy_scope(user, Project) }
+
+    it 'is empty for anonymous users' do
+      expect(Pundit.policy_scope(nil, Project)).to be_empty
+    end
+
+    it 'includes projects a user is allowed to view' do
+      assign_role!(user, :viewer, project)
+      expect(subject).to include(project)
+    end
+
+    it "doesn't include projects a user is not allowed to view" do
+      expect(subject).to be_empty
+    end
+
+    it 'returns all projects for admins' do
+      user.admin = true
+      expect(subject).to include(project)
+    end
+  end
 
   permissions :show? do
     it 'blocks anonymous users' do
