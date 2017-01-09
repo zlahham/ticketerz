7a09799a5cb5a2912ee0fea8673ec22a7a4e71dc
Zaid Al Lahham <zlahham@gmail.com>
2017-01-09 17:38:48 +0200

    Archived users cannot sign into the app

--- spec/features/singing_in_spec.rb
+++ spec/features/singing_in_spec.rb
@@ -12,4 +12,16 @@ feature 'Users that are signed up can sign in' do
     expect(page).to have_content 'Signed in successfully.'
     expect(page).to have_content "Signed in as #{user.email}"
   end
+
+  scenario 'unless they are archived' do
+    user.archive
+
+    visit root_path
+    click_link 'Sign in'
+    fill_in 'Email', with: user.email
+    fill_in 'user_password', with: 'password'
+    click_button 'Sign in'
+
+    expect(page).to have_content 'Your account has been archived.'
+  end
 end
